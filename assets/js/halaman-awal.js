d2c4666f91701f64199d0858b47c82c23a717214
Pasha Raditya Putra <ungu55.pr@gmail.com>
2026-03-15 00:38:09 +0700

    Big Update 2

--- assets/js/halaman-awal.js
+++ assets/js/halaman-awal.js
@@ -2,13 +2,6 @@ const hamburger =
   document.getElementById("hamburger");
 const navMenu =
   document.getElementById("nav-menu");
-const basicLevel =
-  document.getElementById("basic");
-const intermediateLevel = document.getElementById(
-  "intermediate",
-);
-const expertLevel =
-  document.getElementById("expert");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -23,16 +16,4 @@ document
       navMenu.classList.remove("active");
     });
   });
-
-basicLevel.addEventListener("click", function () {
-  window.location.href = "basic.html";
-});
-
-intermediateLevel.addEventListener("click", function () {
-  window.location.href = "intermediate.html";
-});
-
-expertLevel.addEventListener("click", function () {
-  window.location.href = "expert.html";
-});
 
