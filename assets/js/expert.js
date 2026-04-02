8df34519eb598e182f892d29c4e9a45476d37374
Pasha Raditya Putra <ungu55.pr@gmail.com>
2026-04-02 16:49:04 +0700

    50% Expert

--- assets/js/expert.js
+++ assets/js/expert.js
@@ -1,24 +1,64 @@
+const judulHeading = [
+  "Blokir Website Menggunakan DNS Static",
+  "Blokir File Menggunakan Web Proxy",
+];
+const containerHeading =
+  document.querySelectorAll(".submodul");
+
 const hamburger =
   document.getElementById("hamburger");
 const navMenu =
   document.getElementById("nav-menu");
-const basicLevel =
-  document.getElementById("basic");
-const intermediateLevel =
-  document.getElementById("intermediate");
-const expertLevel =
-  document.getElementById("expert");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+if (hamburger && navMenu) {
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+}
+
+containerHeading.forEach((div, index) => {
+  if (judulHeading[index]) {
+    const heading = document.createElement("h2");
+
+    heading.textContent = `${index + 1}. ${judulHeading[index]}`;
+
+    div.prepend(heading);
+  }
 });
 
 document
   .querySelectorAll("nav ul li a")
   .forEach((link) => {
     link.addEventListener("click", () => {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
+      if (hamburger)
+        hamburger.classList.remove("active");
+      if (navMenu)
+        navMenu.classList.remove("active");
     });
   });
+
+document.addEventListener("click", (event) => {
+  if (event.target.id !== "btn-dropdown") return;
+
+  const button = event.target;
+  const dropdownContent =
+    button.nextElementSibling;
+
+  if (
+    dropdownContent &&
+    dropdownContent.classList.contains(
+      "dropdown-content",
+    )
+  ) {
+    dropdownContent.classList.toggle("active");
+    button.textContent =
+      (
+        dropdownContent.classList.contains(
+          "active",
+        )
+      ) ?
+        "Tutup Sub-Modul"
+      : "Buka Sub-Modul";
+  }
+});
